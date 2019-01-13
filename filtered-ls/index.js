450beb06675cc903d25f7f54edadf32a11c16147
Sean Chavez <realseanchavez@gmail.com>
2019-01-13 13:38:18 -0800

    filtered_ls

---
+++ filtered-ls/index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const path = require('path');
+
+const dir = process.argv[2];
+const ext = '.' + process.argv[3];
+
+fs.readdir(dir, (err, files) => {
+  if (err) {
+    console.error(err);
+  } else {
+    files.forEach(file => {
+      if (path.extname(file) === ext) {
+        console.log(file);
+      }
+    });
+  }
+});
